0daf4f1fc0965e710668f0f61f2feb0b9cf4b6c8
jannerdejesus1 <jannerdejesus1@gmail.com>
2026-03-13 07:31:17 -0500

    Revert "registrar materias y notas"

---
+++ registrodeestudiantes.py
@@ -0,0 +1,9 @@
+lista_estudiantes = []
+ 
+def registrar_estudiante():
+    nombre = input("Nombre del estudiante: ")
+    lista_estudiantes.append({"nombre": nombre, "materias": []})
+    print("Estudiante registrado")
+ 
+
+ 
